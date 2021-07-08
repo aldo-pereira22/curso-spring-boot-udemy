7f70226d52ffd09adfaefd9eb497dfebfc3eff22
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-08 18:20:58 -0300

    Projeto criado

---
+++ src/main/java/principal/curso/CursoSpringUdemyApplication.java
@@ -0,0 +1,13 @@
+package principal.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/principal/curso/CursoSpringUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package principal.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
