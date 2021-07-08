3de3d947c65f62efe2fedd14dfa6530adb1fafd5
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-08 18:35:05 -0300

    Entidade Usuário e o seu recurso

---
+++ src/main/java/principal/curso/resources/UserResource.java
@@ -0,0 +1,22 @@
+package principal.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import principal.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		
+		User u = new User(1L, "Aldo", "aldo@gmail.com", "11111", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
