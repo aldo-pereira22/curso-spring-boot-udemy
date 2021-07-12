8f0e45154e5eba73e14a805b6531152e0b5efdfa
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-11 22:18:23 -0300

    Associação de Pagamento com pedido one-to-one

--- src/main/java/principal/curso/config/TestConfig.java
+++ src/main/java/principal/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import principal.curso.entities.Category;
 import principal.curso.entities.Order;
 import principal.curso.entities.OrderItem;
+import principal.curso.entities.Payment;
 import principal.curso.entities.Product;
 import principal.curso.entities.User;
 import principal.curso.entities.enums.OrderStatus;
@@ -87,8 +88,42 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		
+		
 	}
 	
 	
 	
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
