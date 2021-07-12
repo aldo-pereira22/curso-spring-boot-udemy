7a5721e29a6150b49d968799a9ec0dc81e93ca28
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-12 15:18:55 -0300

    Métod update em Usuário

--- src/main/java/principal/curso/services/UserService.java
+++ src/main/java/principal/curso/services/UserService.java
@@ -34,6 +34,23 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User  entity = repository.getOne(id);
+		upDateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void upDateData(User entity, User obj) {
+
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
+	
+	
+	
 }
 
 
