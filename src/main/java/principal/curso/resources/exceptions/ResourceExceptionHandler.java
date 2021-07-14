fa4462da612cb9fb5beef071a932cd6ca5e6e3c2
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-13 21:37:47 -0300

    Exception handler - Delete

--- src/main/java/principal/curso/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/principal/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import principal.curso.services.exceptions.DatabaseException;
 import principal.curso.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -22,4 +23,15 @@ public class ResourceExceptionHandler {
 		
 		return ResponseEntity.status(status).body(err);
 	}
+	
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandartError> database(DatabaseException e, HttpServletRequest  request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandartError err =  new StandartError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+
 }
