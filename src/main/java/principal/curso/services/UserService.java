1a783df92a748e5e9ba3740587fe3be0a963352a
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-13 21:47:27 -0300

    Exception handler - update

--- src/main/java/principal/curso/services/UserService.java
+++ src/main/java/principal/curso/services/UserService.java
@@ -3,6 +3,8 @@ package principal.curso.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -39,17 +41,22 @@ public class UserService {
 			repository.deleteById(id);
 
 		} catch (EmptyResultDataAccessException e) {
-			
+
 			throw new ResourceNotFoundException(id);
-		}catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
 
 	public User update(Long id, User obj) {
+	try {
 		User entity = repository.getOne(id);
 		upDateData(entity, obj);
 		return repository.save(entity);
+		
+	} catch (EntityNotFoundException e) {
+		throw new DatabaseException(e.getMessage());
+	}
 	}
 
 	private void upDateData(User entity, User obj) {
